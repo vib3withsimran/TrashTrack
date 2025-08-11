8cfe7e2a7a36fe99eb08e879d5efb3643250ec34
vib3withsimran <mssimran093@gmail.com>
2025-08-11 15:10:06 +0530

    adding basic code

---
+++ main/app.js
@@ -0,0 +1,18 @@
+let menubtn = document.querySelector(".menu");
+let navlink = document.querySelector(".navlinks");
+let navlinks = document.querySelectorAll(".navlinks a");
+
+menubtn.addEventListener('click', () => {
+    navlink.classList.remove('hidden');
+    navlink.classList.toggle('mobile-screen');
+    menubtn.classList.toggle('active');
+})
+
+// Close menu when a nav link is clicked
+navlinks.forEach(link => {
+    link.addEventListener('click', () => {
+        navlink.classList.toggle('hidden');
+        navlink.classList.remove('mobile-screen');
+        menubtn.classList.remove('active');
+    });
+});
